ea6cfc48f81f4ec8a884ff9c3e5c73d1663c77c2
DatBrainDushan <datbigbraindushan@gmail.com>
2024-08-24 20:09:01 +0300

    Debugged and ready to use e2e/clients/dashboard-vue/App.vue

---
+++ e2e/clients/dashboard-vue/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <dashboard :FirstAttempt="FirstAttempt" />
+</template>
+
+<script>
+import Vue from 'vue'
+import FirstAttempt from '@FirstAttempt/core'
+import { Dashboard } from '@FirstAttempt/vue'
+
+export default {
+  name: 'App',
+  components: {
+    Dashboard,
+  },
+  computed: {
+    FirstAttempt: () => new FirstAttempt(),
+  },
+}
+</script>
+
+<style src="@FirstAttempt/core/dist/style.css"></style>
+<style src="@FirstAttempt/dashboard/dist/style.css"></style>
